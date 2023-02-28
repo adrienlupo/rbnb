3ca60211eac0e1323e98260755bab97f0c115edc
Evan Fabiani <fabiani.evan@icloud.com>
2023-02-28 12:08:00 +0100

    creation resources motorcycles puis controller motorcycles avec index et show

---
+++ app/controllers/motorcycles_controller.rb
@@ -0,0 +1,14 @@
+class MotorcyclesController < ApplicationController
+  before_action :set_motorcycle, only: [:show]
+  def index
+    @motorcycles = Motorcycle.all
+  end
+
+  def show; end
+
+  private
+
+  def set_motorcycle
+    @motorcycle = Motorcycle.find(params[:id])
+  end
+end
