c4a50eae86818a9ff37689392357e1d26aa8c1c6
Remi Buscot <buscot.remi@gmail.com>
2023-02-28 16:35:53 +0100

    seeds_motorbikes

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,11 +1,10 @@
 class Motorcycle < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
   validates :capacity, presence: true, numericality: { only_integer: true }
   validates :category, presence: true
   validates :description, presence: true
   validates :moto_picture_url, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
-  validates :available_dates, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_103905) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_111826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
