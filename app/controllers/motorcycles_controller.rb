44d1431502a552f9be7fef2325aa481a8928aeca
Adrien Lupo <adrien.lupo@protonmail.com>
2023-02-28 18:47:28 +0100

    create fonctionne dans le motorcycle

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -9,19 +9,19 @@ class MotorcyclesController < ApplicationController
     @motorcycle = Motorcycle.new
   end
 
-  # def create
-  #   @user = User.find(params[:user_id])
-  #   @motorcycle = Motorcycle.new(motorcycle_params)
-  #   respond_to do |format|
-  #     if @motorcycle.save
-  #       format.html { redirect_to motorcycle_path(@motorcycle), notice: "Motorcycle was successfully created." }
-  #       # format.json { render :show, status: :created, location: @motorcycle }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       # format.json { render json: @motorcycle.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def create
+    @motorcycle = Motorcycle.new(motorcycle_params)
+    @motorcycle.user = current_user
+    respond_to do |format|
+      if @motorcycle.save
+        format.html { redirect_to motorcycle_path(@motorcycle), notice: "Motorcycle was successfully created." }
+        format.json { render :show, status: :created, location: @motorcycle }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @motorcycle.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def show; end
 
