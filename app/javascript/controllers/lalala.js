66efbf315d54ec98e8a4a8d64b6a7f5907ed68a6
Adrien Lupo <adrien.lupo@protonmail.com>
2023-03-01 16:50:04 +0100

    json error to review mapbox

--- app/javascript/controllers/lalala.js
+++
@@ -1,7 +0,0 @@
-var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-
-mapboxgl.accessToken = 'pk.eyJ1IjoicmJ1c2NvdDAxIiwiYSI6ImNsZWhhMnRnMDBoNTgzeG1jNzFtaTQ0MDUifQ.xS72JMJpzO2eEOX5GiznWA';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
-});
