68d952bd2107c6f8e804f736ecf0bbf4565b3f87
Adrien Lupo <adrien.lupo@protonmail.com>
2023-03-02 16:45:41 +0100

    added a end

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
 
   resources :motorcycles, except: [:index] do
     resources :bookings, only: %i[create new show]
-end
-get "/profil", to: "users#profil"
+  end
+
+  get "/profil", to: "users#profil"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # My bookings >> yakamoto/users/bookings
   # Defines the root path route ("/")
+end
