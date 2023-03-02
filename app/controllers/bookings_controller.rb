b01f07167d1ebab5846f3e91a87eb4f057520a94
Evan Fabiani <fabiani.evan@icloud.com>
2023-03-02 15:54:14 +0100

    Jean-Loup est passé par ici

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+  before_action :set_motorcycle, only: %i[new create]
+
+  def show
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(params_bookings)
+    @booking.motorcycle = @motorcycle
+    @booking.user = current_user
+    @booking.owner_id = @booking.motorcycle.user
+    if @booking.save
+      redirect_to motorcycle_path(@motorcycle)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  private
+
+  def params_bookings
+    params.require(:booking).permit(:end_date, :start_date)
+  end
+
+  def set_motorcycle
+    @motorcycle = Motorcycle.find(params[:motorcycle_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "motorcycles#home"
-  resources :motorcycles, except: [:index]
+
+  resources :motorcycles, except: [:index] do
+    resources :bookings, only: %i[create new show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
