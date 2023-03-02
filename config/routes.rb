e8a356da4b14710b946658b1308fbfe533903163
Adrien Lupo <adrien.lupo@protonmail.com>
2023-03-02 16:30:16 +0100

    added the profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "motorcycles#home"
   resources :motorcycles, except: [:index]
+  # get "users/:id/bookings", to "users#bookings", as :user_bookings
+
+  get "/profil", to: "users#profil"
+end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # My bookings >> yakamoto/users/bookings
   # Defines the root path route ("/")
-end
